dd0ff3380397c6d2f80308483f6442a512c1dc1f
talissoares <talis.soares41@gmail.com>
2019-01-14 10:09:37 -0200

    acho está chegando perto

--- OBI/elevador/elevador.c
+++ OBI/elevador/elevador.c
@@ -12,6 +12,8 @@ int main(){
 	int N;
 	scanf("%d", &N);
 	int caixa[N];
+	int cima[N];
+	int aux2 = 0;
 
     caixa[0] = 0;
 	for (int i = 1; i <= N; ++i){
@@ -30,22 +32,34 @@ int main(){
 		}
 		}
 	}
-
-
+//iniciei um vetor com posições 0
+for (int Q = 0; Q <=N; ++Q){
+	cima[Q] = 0;
+}
+/*
 for (int a = 0; a <=N; ++a){
 p = caixa[a + 1] - caixa[a];
- if( p > 8){
-printf("N\n");
-break;
- }else{
-printf("S\n");
- }
+ if( p  <=  8){
+ cima[a + 1] = caixa[a + 1];
 
 }
-
-
-
-
+*/
+ for (int h = 0 ; h  < N; ++h){
+ 	for (int w = 0; w < N; ++w){
+ 	if (cima[h + 1] < caixa[w + 1]){
+ 		
+ 		if ( cima[h + 1] - caixa[w + 1] <= 8){
+ 		aux2 = cima[h + 1];
+ 		cima[h + 1] = caixa[w +1];
+ 		caixa[w +1] = aux2;
+ 		}
+ 	}
+ 	}
+ }
+for (int o = 0; o <= N; ++N)
+{
+	printf("%d\n", cima[o]);
+}
 
 	return 0;
 
