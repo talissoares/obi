f4b8d66e0394dd8ae46b39741e6101d9b398c43f
talissoares <talis.soares41@gmail.com>
2019-01-15 19:20:10 -0200

    foi caralho

--- OBI/elevador/elevador.c
+++ OBI/elevador/elevador.c
@@ -14,16 +14,17 @@ int main(){
 	int caixa[N];
 	int cima[N];
 	int aux2 = 0;
+	int some = 0;
 
-    caixa[0] = 0;
-	for (int i = 1; i <= N; ++i){
+    
+	for (int i = 0; i < N; ++i){
 		scanf("%d", &caixa[i]);
 	}
 
 	int aux = 0;
 //lendo o vetor  novamente para organizar, logo depois
-	for (int i = 1; i <= N; ++i){
-		for (int j = 1; j < N; ++j){
+	for (int i = 0; i < N; ++i){
+		for (int j = 0; j < N; ++j){
 		
 		if (caixa[i] < caixa[j]){
 			aux = caixa[i];
@@ -33,7 +34,7 @@ int main(){
 		}
 	}
 //iniciei um vetor com posições 0
-for (int Q = 0; Q <=N; ++Q){
+for (int Q = 0; Q < N; ++Q){
 	cima[Q] = 0;
 }
 /*
@@ -44,23 +45,50 @@ p = caixa[a + 1] - caixa[a];
 
 }
 */
+
+// posição de cima 0 é 4
+
  for (int h = 0 ; h  < N; ++h){
  	for (int w = 0; w < N; ++w){
- 	if (cima[h + 1] < caixa[w + 1]){
- 		
- 		if ( cima[h + 1] - caixa[w + 1] <= 8){
- 		aux2 = cima[h + 1];
- 		cima[h + 1] = caixa[w +1];
- 		caixa[w +1] = aux2;
+ 	if (cima[w] < caixa[h]){	
+ 		if ( ( caixa[h] - cima[w]) <= 8){
+ 		aux2 = caixa[h];
+ 	    caixa[h]=cima[w];
+ 		cima[w] = aux2;
+
  		}
+
+ 		
  	}
  	}
  }
-for (int o = 0; o <= N; ++N)
+
+
+for (int Q = 0; Q < N; ++Q)
 {
-	printf("%d\n", cima[o]);
+
+some = caixa[Q] + some;
 }
 
+if(some == 0){
+	printf("S\n");
+}else{
+	printf("N\n");
+}
+
+//quando for testar, só desativar o comentário abaixo.
+/*
+for (int Q = 0; Q < N; ++Q)
+{
+	printf("%d,", cima[Q]);
+}
+printf("\n");
+
+for (int R = 0; R < N; ++R)
+{
+	printf("%d,", caixa[R]);
+}
+*/
 	return 0;
 
 }
